432f7298cf7c190ec5a3c00738998bac2446c203
Atomiz <tytunipex@gmail.com>
2026-04-28 10:59:24 +0300

    JsonExtensions: updated implementation

--- Editor/Extensions/JsonExtensions.cs
+++ Editor/Extensions/JsonExtensions.cs
@@ -1,30 +1,29 @@
-﻿#if DEVELOPMENT_TOOLS_NEWTONSOFT_JSON
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+#if DEVELOPMENT_TOOLS_EDITOR_NEWTONSOFT_JSON
 using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-#else
-using UnityEngine;
 #endif
-using JetBrains.Annotations;
 
 namespace DevelopmentTools.Editor.Extensions {
 
     public static class JsonExtensions {
 
-        // TODO update from TT3D
+        public static void FromJsonOverwrite(this Object objectToOverwrite, string json) => JsonUtility.FromJsonOverwrite(json, objectToOverwrite);
 
-#if DEVELOPMENT_TOOLS_NEWTONSOFT_JSON
-        #region NEWTONSOFT JSON
+        public static void FromJsonOverwrite(this string json, Object objectToOverwrite) => JsonUtility.FromJsonOverwrite(json, objectToOverwrite);
 
+#if DEVELOPMENT_TOOLS_EDITOR_NEWTONSOFT_JSON
+
+        /// Automatically uses unity's JsonUtility for unity <see cref="Object"/>s and newtonsoft's JsonConvert for the rest
         [Pure]
-        public static T FromJSON<T>(this string json) => JsonConvert.DeserializeObject<T>(json);
+        public static T FromJson<T>(this string json) => typeof(Object).IsAssignableFrom(typeof(T)) ? JsonUtility.FromJson<T>(json) : JsonConvert.DeserializeObject<T>(json, SafeContractResolver.Settings);
 
+        /// <inheritdoc cref="FromJson"/>
         [Pure]
-        public static string ToJSON<T>(this T obj, bool pretty = false) => JsonConvert.SerializeObject(obj, pretty ? Formatting.Indented : Formatting.None,
-            new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore, ContractResolver = new SafeContractResolver() });
-
-        [Pure]
-        public static T CloneJSON<T>(this T obj) => JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(obj));
+        public static string ToJson<T>(this T obj, bool prettyPrint = false) => typeof(Object).IsAssignableFrom(typeof(T)) ? JsonUtility.ToJson(obj, prettyPrint) : JsonConvert.SerializeObject(obj, prettyPrint ? Formatting.Indented : Formatting.None, SafeContractResolver.Settings);
 
         private class SafeContractResolver : DefaultContractResolver {
 
@@ -41,29 +40,37 @@ namespace DevelopmentTools.Editor.Extensions {
                     }
                 };
 
+                // include privates
+                // FieldInfo f = m as FieldInfo;
+                // if (f != null)
+                //     p.Readable = p.Writable = true;
+
                 return p;
             }
 
-        }
+            public static JsonSerializerSettings Settings => new() {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                ContractResolver = new SafeContractResolver(),
+                // TypeNameHandling      = TypeNameHandling.All
+            };
 
-        #endregion
+        }
 
 #else
 
-        #region JSON UTILITY
-
+        /// Uses Unity's JsonUtility.<br/>Install Newtonsoft JSON to automatically use unity's JsonUtility for unity <see cref="Object"/>s and newtonsoft's JsonConvert for the rest
         [Pure]
-        public static T FromJSON<T>(this string json) => JsonUtility.FromJson<T>(json);
+        public static T FromJson<T>(this string json) => JsonUtility.FromJson<T>(json);
 
+        /// <inheritdoc cref="FromJson"/>
         [Pure]
-        public static string ToJSON<T>(this T obj, bool pretty = false) => JsonUtility.ToJson(obj, pretty);
-
-        [Pure]
-        public static T CloneJSON<T>(this T obj) => JsonUtility.FromJson<T>(JsonUtility.ToJson(obj));
-
-        #endregion
+        public static string ToJson<T>(this T obj, bool prettyPrint = false) => JsonUtility.ToJson(obj, prettyPrint);
 
 #endif
+
+        /// <inheritdoc cref="FromJson"/>
+        [Pure]
+        public static T CloneJson<T>(this T obj) => obj.ToJson().FromJson<T>();
 
     }
 
