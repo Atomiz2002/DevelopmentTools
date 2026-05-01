9d0b1032a19cfec51622a3475a8e27174d1e12da
Atomiz <tytunipex@gmail.com>
2026-05-01 23:32:10 +0300

    InfoContainer: key - Type -> object

--- Runtime/InfoContainer.cs
+++ Runtime/InfoContainer.cs
@@ -9,13 +9,13 @@ namespace DevelopmentTools {
     public static class InfoContainer {
 
         // using object instead of List<Func<object, object((T))>> for performance reasons
-        private static readonly Dictionary<Type, Dictionary<Type, object>> registeredExtractors = new();
-        private static readonly Dictionary<Type, Dictionary<Type, object>> registeredModifiers  = new();
+        private static readonly Dictionary<object, Dictionary<Type, object>> registeredExtractors = new();
+        private static readonly Dictionary<object, Dictionary<Type, object>> registeredModifiers  = new();
 
-        public static void RegisterInfoExtractor<T, TResult>(Type type, Func<T, TResult> extractor) {
-            if (!registeredExtractors.TryGetValue(type, out Dictionary<Type, object> typedExtractors)) {
+        public static void RegisterInfoExtractor<T, TResult>(object key, Func<T, TResult> extractor) {
+            if (!registeredExtractors.TryGetValue(key, out Dictionary<Type, object> typedExtractors)) {
                 typedExtractors = new();
-                registeredExtractors.Add(type, typedExtractors);
+                registeredExtractors.Add(key, typedExtractors);
             }
 
             if (!typedExtractors.TryGetValue(typeof(TResult), out object extractors)) {
@@ -26,10 +26,10 @@ namespace DevelopmentTools {
             ((List<Func<T, TResult>>) extractors).Add(extractor);
         }
 
-        public static void RegisterInfoModifier<T>(Type type, Func<T, T> modifier) {
-            if (!registeredModifiers.TryGetValue(type, out Dictionary<Type, object> typedModifiers)) {
+        public static void RegisterInfoModifier<T>(object key, Func<T, T> modifier) {
+            if (!registeredModifiers.TryGetValue(key, out Dictionary<Type, object> typedModifiers)) {
                 typedModifiers = new();
-                registeredModifiers.Add(type, typedModifiers);
+                registeredModifiers.Add(key, typedModifiers);
             }
 
             if (!typedModifiers.TryGetValue(typeof(T), out object modifiers)) {
@@ -40,21 +40,21 @@ namespace DevelopmentTools {
             ((List<Func<T, T>>) modifiers).Add(modifier);
         }
 
-        public static void ExtractAndModifyInfo<T>(Type type, ref string input, ref List<T> output, bool add = true) {
-            ExtractInfo(type, input, ref output, add);
-            ModifyInfo(type, ref input);
+        public static void ExtractAndModifyInfo<T>(object key, ref string input, ref List<T> output, bool add = true) {
+            ExtractInfo(key, input, ref output, add);
+            ModifyInfo(key, ref input);
         }
 
-        public static void ExtractInfo<T>(Type type, object input, ref List<T> output, bool add = true) {
+        public static void ExtractInfo<T>(object key, object input, ref List<T> output, bool add = true) {
             if (add)
-                output.AddRange(ExtractInfo<T>(type, input));
+                output.AddRange(ExtractInfo<T>(key, input));
             else
-                output = ExtractInfo<T>(type, input).ToList();
+                output = ExtractInfo<T>(key, input).ToList();
         }
 
         [Pure]
-        public static IEnumerable<T> ExtractInfo<T>(Type type, object input) {
-            if (!registeredModifiers.TryGetValue(type, out Dictionary<Type, object> typedExtractors))
+        public static IEnumerable<T> ExtractInfo<T>(object key, object input) {
+            if (!registeredModifiers.TryGetValue(key, out Dictionary<Type, object> typedExtractors))
                 yield break;
 
             if (!typedExtractors.TryGetValue(typeof(T), out object extractors))
@@ -64,11 +64,11 @@ namespace DevelopmentTools {
                 yield return extractor.SafeInvoke(input);
         }
 
-        public static void ModifyInfo<T>(Type type, ref T input) => input = ModifyInfo(type, input);
+        public static void ModifyInfo<T>(object key, ref T input) => input = ModifyInfo(key, input);
 
         [Pure]
-        public static T ModifyInfo<T>(Type type, T input) {
-            if (!registeredModifiers.TryGetValue(type, out Dictionary<Type, object> typedModifiers))
+        public static T ModifyInfo<T>(object key, T input) {
+            if (!registeredModifiers.TryGetValue(key, out Dictionary<Type, object> typedModifiers))
                 return input;
 
             if (!typedModifiers.TryGetValue(typeof(T), out object modifiers))
